16667898ef54b1c457c251fde21748478b071d7d
sirdav1d <ddavid.diniz@gmail.com>
2023-04-13 10:45:13 -0300

    fix: :art: main in src

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
+import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './src/Styles/GlobalStyle';
+import { theme } from './Styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
